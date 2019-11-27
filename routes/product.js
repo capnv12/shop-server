88b444dd853bee194b805d7a7da8b03014966be1
Mare Robert <capnv12@gmail.com>
2019-11-27 20:49:44 +0200

    aPI almost ready

--- routes/product.js
+++ routes/product.js
@@ -2,13 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
-const { create, productById, read, remove, update } = require('../controllers/product')
+const { create, productById, read, remove, update, list, listRelated, listCategories,listBySearch, photo } = require('../controllers/product')
 const { userById } = require('../controllers/user')
 
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.get('/product/:productId', read)
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove)
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update)
+router.get('/products', list)
+router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
+router.post("/products/by/search", listBySearch);
+router.get('/product/photo/:productId', photo)
 
 router.param('userId', userById)
 router.param('productId', productById)
